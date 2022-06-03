c861f5720ecf1d033de5a0fc72b9db92943df2ec
Zarko Radevic <zara@1881.no>
2022-06-03 21:48:41 +0200

    Added jest unit testing

--- ClientCode/src/Forms/AccountForm.ts
+++ ClientCode/src/Forms/AccountForm.ts
@@ -1,13 +1,22 @@
-import moment from "moment";
-
 export class AccountForm {
   static async onload(context: Xrm.Events.EventContext): Promise<void> {
-    const now = moment().format();
-    context
-      .getFormContext()
-      .getAttribute("name")
-      .addOnChange(() => {
-        console.log(`Name on change ${now}`);
-      });
+    context.getFormContext().getAttribute("websiteurl").addOnChange(AccountForm.onWebsiteChanged);
+  }
+  static onWebsiteChanged(context: Xrm.Events.EventContext): void {
+    const formContext = context.getFormContext();
+    const websiteAttribute = formContext.getAttribute("websiteurl");
+    const websiteRegex = /^(https?:\/\/)?([\w\d]+\.)?[\w\d]+\.\w+\/?.+$/g;
+    let isValid = true;
+    if (websiteAttribute && websiteAttribute.getValue()) {
+      const match = websiteAttribute.getValue().match(websiteRegex);
+      isValid = match != null;
+    }
+    websiteAttribute.controls.forEach((c) => {
+      if (isValid) {
+        (c as Xrm.Controls.StringControl).clearNotification("websiteurl");
+      } else {
+        (c as Xrm.Controls.StringControl).setNotification("Invalid Website Address", "websiteurl");
+      }
+    });
   }
 }
