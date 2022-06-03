783b7c42afcf31a7ffe888a062cdbee26bc44130
Zarko Radevic <zara@1881.no>
2022-06-03 20:32:03 +0200

    Added Webpack bundling

--- ClientCode/src/Forms/AccountForm.ts
+++ ClientCode/src/Forms/AccountForm.ts
@@ -1,10 +1,13 @@
+import moment from "moment";
+
 export class AccountForm {
   static async onload(context: Xrm.Events.EventContext): Promise<void> {
+    const now = moment().format();
     context
       .getFormContext()
       .getAttribute("name")
       .addOnChange(() => {
-        console.log("name onchange");
+        console.log(`Name on change ${now}`);
       });
   }
 }
